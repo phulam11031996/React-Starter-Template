1723833886ab61185331091c11e7a61d05591f46
phulam11031996 <phulam11031996@gmail.com>
2023-08-16 19:11:32 -0700

    implement and test crud for account microservice

--- backend-account/dist/common/httpCode.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NOT_FOUND_404 = exports.INTERNAL_ERROR_500 = exports.NO_CONTENT_204 = exports.CREATE_201 = exports.OK_200 = void 0;
-exports.OK_200 = 200;
-exports.CREATE_201 = 201;
-exports.NO_CONTENT_204 = 204;
-exports.INTERNAL_ERROR_500 = 500;
-exports.NOT_FOUND_404 = 404;
